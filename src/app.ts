56cd3d81e296325e9ca46bcea25f839bfdeb688d
Daniel Restrepo <juanrestrepowebdev@gmail.com>
2024-05-07 21:01:51 -0500

    Serve index.html from rest server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 import http from 'http';
+import fs from 'fs';
 
 const server = http.createServer((req, res) => {
 
     console.log(req.url)
-    res.write('Hello world');
-    res.end();
+    /* res.writeHead(200, { 'Content-Type': 'text/html'});
+    res.write(`<h1>URL ${ req.url }</h1>`);
+    res.end(); */
+
+    /* const data = { name: 'Jhon Doe', city: 'New York'}
+    res.writeHead(200, { 'Content-Type': 'application/json'});
+    res.end( JSON.stringify(data)); */
+
+    if ( req.url === '/') {
+        const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end( htmlFile );
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html'});
+        res.end();
+    }
 })
 
 server.listen(8080, () => {
