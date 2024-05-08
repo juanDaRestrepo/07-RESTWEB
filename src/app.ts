2f65fd3c73f13ddf50674eb96359a9d6c3bfd843
Daniel Restrepo <juanrestrepowebdev@gmail.com>
2024-05-07 20:44:47 -0500

    First rest server response

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
+import http from 'http';
 
+const server = http.createServer((req, res) => {
 
+    console.log(req.url)
+    res.write('Hello world');
+    res.end();
+})
+
+server.listen(8080, () => {
+    console.log('Server running on port 8080')
+});
