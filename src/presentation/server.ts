5a1ac4264c5c69dcd885b5f1facbbb965a515a5b
Daniel Restrepo <juanrestrepowebdev@gmail.com>
2024-05-13 13:09:33 -0500

    Serve spa with express

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 
 export class Server {
@@ -11,6 +12,11 @@ export class Server {
 
         // Public Folder
         this.app.use( express.static('public'));
+
+        this.app.get('*', (req, res) => {
+            const indexPath = path.join(__dirname + '../../../public/index.html');
+            res.sendFile(indexPath);
+        })
 
         this.app.listen(8080, () => {
             console.log(`Server running on port ${8080}`);
