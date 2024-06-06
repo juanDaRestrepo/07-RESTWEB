246c3adaefbd767f5c40c45e646365176b3ad759
Daniel Restrepo <juanrestrepowebdev@gmail.com>
2024-06-06 17:44:11 -0500

    Create todo entity

---
+++ src/domain/entities/todo.entity.ts
@@ -0,0 +1,15 @@
+
+
+export class TodoEntity {
+
+    constructor(
+        public id: number,
+        public text: string,
+        public completedAt? : Date|null
+    ){}
+
+    get isCompleted() {
+        return !!this.completedAt
+    }
+
+}
