fe361e28f2417adaf741ed437bb776a3d1a0eb32
Daniel Restrepo <juanrestrepowebdev@gmail.com>
2024-06-06 20:24:31 -0500

    Create todo datasource implementation

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -12,4 +12,21 @@ export class TodoEntity {
         return !!this.completedAt
     }
 
+    public static fromObject(object: {[key: string]: any}) {
+        const { id, text, completedAt } = object;
+        if ( !id ) throw 'Id is required';
+        if ( !text ) throw 'text is required';
+
+        let newCompletedAt;
+        if ( completedAt ) {
+            newCompletedAt = new Date(completedAt);
+            if ( isNaN( newCompletedAt.getTime() ) ) {
+                throw 'CompletedAt is not a valid date';
+            }
+        }
+
+        return new TodoEntity(
+            id, text, completedAt
+        )
+    }
 }
