e8eddfd1bc005140ed0fb137751fff95436aedf3
Daniel Restrepo <juanrestrepowebdev@gmail.com>
2024-06-15 10:42:46 -0500

    Create domain driven design controller

--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { TodosController } from "./controllers";
+import { TodoDatasourceImpl } from "../../infraestructure/datasource/todo.datasource.impl";
+import { TodoRepositoryImpl } from "../../infraestructure/repositories/todo.repository.impl";
 
 
 export class TodoRoutes {
 
     static get routes(): Router {
         const router = Router();
-        const todoController = new TodosController()
+
+        const datasource = new TodoDatasourceImpl();
+        const todoRepository = new TodoRepositoryImpl(datasource);
+        const todoController = new TodosController(todoRepository);
 
         router.get('/', todoController.getTodos );
         router.get('/:id', todoController.getTodoById );
